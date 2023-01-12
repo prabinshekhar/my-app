db3221ffa5b24f5550285b69ea59bdd53230bf60
prabinshekhar <3199beingprabin@gmail.com>
2023-01-12 16:02:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import { Basictable } from './components/Basictable';
+
+function App() {
+  return (
+    <div className="App">
+      <Basictable/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/columns.js
@@ -0,0 +1,27 @@
+export const COLUMNS = [
+    {
+        Header : 'Id',
+        accessor : 'id'
+    },
+    {
+        Header : 'First Name',
+        accessor:'first_name'
+    },
+    {
+        Header : 'Last Name',
+        accessor : 'last_name'
+    },
+    {
+        Header : 'Email',
+        accessor : 'email'
+    },
+    {
+        Header : 'Age',
+        accessor : 'age'
+    },
+    {
+        Header : 'Country',
+        accessor : 'country'
+    }
+
+]
